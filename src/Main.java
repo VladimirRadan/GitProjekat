9ddf023bdc088223cbd022bda942318bf14ef1f4
vlada <vladavr3@gmail.com>
2023-01-05 18:47:30 +0100

    added some lines to Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
-
+        System.out.println("Promena 1");
+        System.out.println("Promena 2");
+        System.out.println("Promena 3");
+        System.out.println("Promena 4");
+        System.out.println("Promena 5");
     }
 
 
