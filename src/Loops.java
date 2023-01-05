89bde0601a53c75502fb03417133ad14996e6c5c
vlada <vladavr3@gmail.com>
2023-01-05 18:40:04 +0100

    Initial commit

---
+++ src/Loops.java
@@ -0,0 +1,51 @@
+
+
+public class Loops {
+
+    public static void main(String[] args) {
+        System.out.println("Promena 1");
+        System.out.println("Promena 2");
+        System.out.println("Promena 3");
+        System.out.println("Promena 4");
+        System.out.println("Promena 5");
+    }
+
+
+
+
+
+
+
+
+
+    //server -  github ---------------------- main/master
+    //                 ------------------------- feature/add-new-changes --> pull/merge request
+
+
+    //local git init ------------------------- main/master
+
+    //git pull origin/main
+    //git branch feature/add-new-changes
+    //git checkout feature/add-new-changes
+
+    // ------------------------- feature/add-new-changes
+    // git add . - dodavanje fajlova za commit
+
+    // git commit -m "adding new changes regarding main method" -------------> server
+
+    // git push origin/feature/add-new-changes
+
+    //git checkout master/main
+
+    // git pull origin/main
+
+
+
+
+
+
+
+
+
+
+}
